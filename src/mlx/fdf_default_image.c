a599dd9fff8635bfd3e808d4f2e2c47ccba1de6d
Killian <oss@xtrm.me>
2023-12-21 21:26:41 +0100

    Image stuff :D

--- src/mlx/fdf_default_image.c
+++ src/mlx/fdf_default_image.c
@@ -6,7 +6,7 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 00:33:59 by kiroussa          #+#    #+#             */
-/*   Updated: 2023/12/20 13:36:55 by kiroussa         ###   ########.fr       */
+/*   Updated: 2023/12/20 19:45:22 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,21 @@ void	*fdf_empty_image(t_mlx_container *data, int width, int height)
 	void			*img;
 	int				x;
 	int				y;
+	int				color;
 
 	img = mlx_new_image(data->mlx, width, height);
 	if (!img)
 		return (NULL);
-	x = 0;
 	y = 0;
 	while (y < height)
 	{
+		x = 0;
 		while (x < width)
 		{
-			mlx_set_image_pixel(data->mlx, img, x, y, 0xFF000000);
+			color = 0xFF0000FF;
+			if (x >= width / 2 && y >= height / 2)
+				color = 0xFF00FF00;
+			mlx_set_image_pixel(data->mlx, img, x, y, color);
 			x++;
 		}
 		y++;
