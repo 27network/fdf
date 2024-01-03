f878b9271e5e46956d236270905b58b61d81f034
Killian <oss@xtrm.me>
2024-01-03 17:02:21 +0100

    🐛 fix(fuck): fuck

--- src/render/mlx/fdf_mlx_update.c
+++ src/render/mlx/fdf_mlx_update.c
@@ -6,7 +6,7 @@
 /*   By: kiroussa <oss@xtrm.me>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 19:36:46 by kiroussa          #+#    #+#             */
-/*   Updated: 2024/01/03 16:15:58 by kiroussa         ###   ########.fr       */
+/*   Updated: 2024/01/03 17:00:15 by kiroussa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,11 @@
 void	fdf_mlx_update(t_mlx_container *data)
 {
 	if (!data->scene_rendered || !data->hud_rendered)
+	{
+		ft_printf("Clearing!\n");
 		mlx_clear_window(data->mlx, data->window);
-	
-		/*
-	if (!data->scene_rendered && false)
+	}
+	if (!data->scene_rendered)
 	{
 		ft_printf("Rendering scene...");
 		fdf_clear_image(data, data->scene, data->width, data->height);
@@ -29,7 +30,6 @@ void	fdf_mlx_update(t_mlx_container *data)
 		ft_printf(" done!\n");
 	}
 	mlx_put_image_to_window(data->mlx, data->window, data->scene, 0, 0);
-	*/
 	if (!data->hud_rendered)
 	{
 		ft_printf("Rendering hud...");
